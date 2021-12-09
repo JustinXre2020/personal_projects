cb1aaf53501d12448f9a0705b533e8e6a0730f43
JustinXre2020 <66919694+JustinXre2020@users.noreply.github.com>
2021-12-09 00:29:17 -0600

    Update Mosaic_Code.py

--- Adding Mosaics to the Image/Mosaic_Code.py
+++ Adding Mosaics to the Image/Mosaic_Code.py
@@ -1,3 +1,14 @@
+def load_img(filename):
+    img = Image.open(filename)
+    pil_img = img.convert('RGB')
+    arr = np.array(pil_img.getdata(), dtype=np.uint8).reshape(pil_img.height, pil_img.width, 3)
+    return [ [ (int(p[0]),int(p[1]),int(p[2])) for p in row ] for row in arr ]
+
+def save_img(img, filename):
+    arr = np.asarray(img, dtype=np.uint8)
+    pil_img = Image.fromarray(arr)
+    pil_img.save(filename, format='png')
+
 def pixelization(image, length, percentage_change):
     for i in range(0, len(image), length):
         for j in range(len(image[0]) - 1, int((1 - percentage_change*0.01)*len(image[0])), -length):
@@ -15,6 +26,5 @@ def pixelization(image, length, percentage_change):
             for ii in range(i, i + length):
                 for jj in range(j, j - length, -1):
                     image[ii][jj] = (int(new_r), int(new_g), int(new_b))
-    return image
 
 
