7bb251424633767fd9db74576ab3a43df741fa61
JustinXre2020 <66919694+JustinXre2020@users.noreply.github.com>
2021-12-09 00:21:10 -0600

    Update Mosaic_Code.py

--- Adding Mosaics to the Image/Mosaic_Code.py
+++ Adding Mosaics to the Image/Mosaic_Code.py
@@ -1,26 +1,19 @@
-from CSE8AImage import *
-
-def pixelization(image, square_size, column_percentage):
-    # TODO
-    for row in range(0, height(image), square_size):
-        for column in range(width(image) - 1, int((1 - column_percentage*0.01)*width(image)), -square_size):
+def pixelization(image, length, percentage_change):
+    for i in range(0, len(image), length):
+        for j in range(len(image[0]) - 1, int((1 - percentage_change*0.01)*len(image[0])), -length):
             r = 0
             g = 0
             b = 0
-            for sub_row in range(row, row + square_size):
-                for sub_col in range(column, column - square_size, -1):
-                    r += image[sub_row][sub_col][0]
-                    g += image[sub_row][sub_col][1]
-                    b += image[sub_row][sub_col][2]
-            new_r = r/(square_size)**2
-            new_g = g/(square_size)**2
-            new_b = b/(square_size)**2
-            for sub_row in range(row, row + square_size):
-                for sub_col in range(column, column - square_size, -1):
-                    image[sub_row][sub_col] = (int(new_r), int(new_g), int(new_b))
-
-image = load_img('input.png')
-pixelization(image, 10, 60)
-save_img(image, 'pixelized.png')
+            for ii in range(i, i + length):
+                for jj in range(j, j - length, -1):
+                    r += image[ii][jj][0]
+                    g += image[ii][jj][1]
+                    b += image[ii][jj][2]
+            new_r = r/(length)**2
+            new_g = g/(length)**2
+            new_b = b/(length)**2
+            for ii in range(i, i + length):
+                for jj in range(j, j - length, -1):
+                    image[ii][jj] = (int(new_r), int(new_g), int(new_b))
 
 
