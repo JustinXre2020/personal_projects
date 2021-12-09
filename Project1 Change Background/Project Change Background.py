5770b23f98e2a01632cb6536acd2f073cfd2c877
JustinXre2020 <66919694+JustinXre2020@users.noreply.github.com>
2021-12-09 00:55:05 -0600

    Update Project Change Background.py

--- Project1 Change Background/Project Change Background.py
+++ Project1 Change Background/Project Change Background.py
@@ -4,9 +4,14 @@ from PIL import Image
 def convert(filename):
     img = Image.open(filename)
     pil_img = img.convert('RGB')
-    arr = np.array(pil_img.getdata(), dtype=np.uint8).reshape(pil_img.height, pil_img
-.width, 3)
-    return [ [ (int(p[0]),int(p[1]),int(p[2])) for p in row ] for row in arr ]
+    arr_img = np.array(pil_img.getdata(), dtype=np.uint8).reshape(pil_img.height, pil_img.width, 3)
+    l = []
+    for row in arr_img:
+	col = []
+	for pixel in row:
+		col.append((int(pixel[0]),int(pixel[1]),int(pixel[2]))
+	l.append(col)
+    return l
 
 
 # Only accept the condition that the size of person image is smaller than the background image
